5068348c1110bee40fc485db01607f88ddb93d60
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-19 23:40:54 +0530

    map part completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // Your location
-    const loc = { lat: 42.361145, lng: -71.057083 };
+    const loc = { lat: 26.113580, lng: 85.907620 };
     // Centered map on location
     const map = new google.maps.Map(document.querySelector('.map'), {
       zoom: 14,
